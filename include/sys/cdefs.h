90233d96f0a98b9557248aae0182c8917a17aa7b
Richard Sandiford <richard@codesourcery.com>
2006-09-07 17:10:20 +0000

    Initial commit
    git-svn-id: http://eglibc.org/svn/trunk/localedef@168 7b3dc134-2b1b-0410-93df-9e9f96275f8d

---
+++ include/sys/cdefs.h
@@ -0,0 +1,27 @@
+#ifdef HAVE_SYS_CDEFS_H
+#include_next <sys/cdefs.h>
+#endif
+
+#ifndef __THROW
+#define __THROW
+#endif
+
+#ifndef __BEGIN_DECLS
+#define __BEGIN_DECLS
+#endif
+
+#ifndef __END_DECLS
+#define __END_DECLS
+#endif
+
+#ifndef __BEGIN_NAMESPACE_STD
+#define __BEGIN_NAMESPACE_STD
+#endif
+
+#ifndef __END_NAMESPACE_STD
+#define __END_NAMESPACE_STD
+#endif
+
+#ifndef __flexarr
+#define __flexarr [0]
+#endif
